68869b2b6686bfd3d17245fafbae73ddb42bab12
Felipe Motta <felipe.motta@roche.com>
2018-06-09 12:14:29 +0200

    Two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './Products/product-list.component';
@@ -12,7 +13,8 @@ import { WelcomeComponent } from './Home//welcome.component';
     WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
