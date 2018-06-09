a57c60ffb7010a89ac57abc99fa2b9c9e963acc3
Felipe Motta <felipe.motta@roche.com>
2018-06-09 14:30:16 +0200

    Create nested component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './Products/product-list.component';
-import { WelcomeComponent } from './Home//welcome.component';
 import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
+import { StartComponent } from './shared/start.component';
+import { WelcomeComponent } from './Home//welcome.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacePipe,
+    StartComponent,
     WelcomeComponent
   ],
   imports: [
