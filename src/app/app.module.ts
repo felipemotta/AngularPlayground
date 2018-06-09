892a17b63a25b16ee45bab4f93d6a12771509871
Felipe Motta <felipe.motta@roche.com>
2018-06-09 13:26:51 +0200

    Custom pipe implementation convertToSpace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './Products/product-list.component';
 import { WelcomeComponent } from './Home//welcome.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    ConvertToSpacesPipe,
     WelcomeComponent
   ],
   imports: [
